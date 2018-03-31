d1ab85c22687645cf45360f7962e607ac7ea81ae
Sam Roquitte <samroq@yahoo.com>
2018-03-31 15:56:26 -0500

    Stepper is now working

--- stepperTesting/stepperTesting.ino
+++ stepperTesting/stepperTesting.ino
@@ -11,19 +11,20 @@ void setup()
   pinMode(5,OUTPUT); // Step
   pinMode(4,OUTPUT); // Dir
   digitalWrite(3,LOW); // Set Enable low
+  digitalWrite(4, LOW);
 }
 
 void loop() 
 {
   digitalWrite(3,LOW); // Set Enable low
-  digitalWrite(4,HIGH); // Set Dir high
+//  digitalWrite(4,HIGH); // Set Dir high
   Serial.println("Loop 200 steps (1 rev)");
-  for(x = 0; x < 200; x++) // Loop 200 times
+  for(x = 0; x < 32; x++) // Loop 200 times
   {
     digitalWrite(5,HIGH); // Output high
     delay(10); // Wait
     digitalWrite(5,LOW); // Output low
-    delay(100); // Wait
+//    delay(100); // Wait
   }
   Serial.println("Pause");
   delay(1000); // pause one second
