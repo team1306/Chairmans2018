12d2caae667305a9c59e5280f9340bf6258b1239
Mateo Silver <mateonsilver@gmail.com>
2018-03-31 12:04:20 -0500

    Another Sketch to try to get the stepper to work

---
+++ stepperTesting.ino
@@ -0,0 +1,30 @@
+// Run a A4998 Stepstick from an Arduino UNO.
+// Paul Hurley Aug 2015
+int x; 
+#define BAUD (9600)
+
+
+void setup() 
+{
+  Serial.begin(BAUD);
+  pinMode(6,OUTPUT); // Enable
+  pinMode(5,OUTPUT); // Step
+  pinMode(4,OUTPUT); // Dir
+  digitalWrite(6,LOW); // Set Enable low
+}
+
+void loop() 
+{
+  digitalWrite(6,LOW); // Set Enable low
+  digitalWrite(4,HIGH); // Set Dir high
+  Serial.println("Loop 200 steps (1 rev)");
+  for(x = 0; x < 200; x++) // Loop 200 times
+  {
+    digitalWrite(5,HIGH); // Output high
+    delay(10); // Wait
+    digitalWrite(5,LOW); // Output low
+    delay(100); // Wait
+  }
+  Serial.println("Pause");
+  delay(1000); // pause one second
+}
