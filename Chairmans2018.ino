c2f829d3ab6e0e6e785be7de90a8fd748ea3d9e7
Sam Roquitte <samroq@yahoo.com>
2018-04-02 23:22:07 -0500

    Fixed LEDs and trellis

--- Chairmans2018.ino
+++ Chairmans2018.ino
@@ -5,17 +5,14 @@
 // Comment out for prod to save memory
 //#define DEBUG 1
 
-int loopi = 0;
-
 // LEDs
 const int LED_PIN = 6;
 int isFire = false;
-const int LED_START = 31;
-const int NUM_LEDS = 250; // TODO: figure this out
+const int LED_START = 30;
+const int NUM_LEDS = 231;
 
 // Hex LEDs
 const int HEX_LED_START = 211;  // TODO: figure this out
-const int NUM_HEX_LEDS = 20;
 const int HEX_LED_R = 255;
 const int HEX_LED_G = 255;
 const int HEX_LED_B = 255;
@@ -37,6 +34,7 @@ const int TRELLIS_NUM_KEYS = 16;
 const int STEPPER_ENABLE_PIN = 3;
 const int STEPPER_PIN = 5;
 const int STEPPER_DIR_PIN = 4;
+const bool STEPPER_FORWARD = 0;
 int currentStepper = 0;
 
 CRGB leds[NUM_LEDS];
@@ -54,6 +52,7 @@ void setup() {
   FastLED.addLeds<NEOPIXEL,LED_PIN>(leds, NUM_LEDS);
   setAll(0, 0, 0);
   delay(100);
+  setHexLEDs(true);
 
   #ifdef DEBUG
     Serial.println("LED setup success");
@@ -79,55 +78,80 @@ void setup() {
     Serial.println("Stepper setup success");
     Serial.println("Startup success\n");
   #endif
-  setStepper(1);
 }
 
 void loop() {
   delay(30);
-  loopi++;
-//  Serial.print(checkTrellis());
-  switch(checkTrellis()) {
-    // First row
-    case 0: // Turn to Ignitions
-      setStepper(1);
-      setThermo(1);
-      setHexLEDs(true);
-    break;
-    case 1: // Thermo level 1
-      setThermo(2);
-    break;
-    case 2: // Turns to Creativity && Thermo level 2 && Fan dial 1
-      setThermo(3);
-      setStepper(1);
-    break;
-    case 3: // Turn to Oppertunity && Thermo level 3 && Fan dial 2
-      setThermo(4);
-      setStepper(1); 
-    break;
+  if (trellis.readSwitches()) {
+    #ifdef DEBUG
+      Serial.println();
+      Serial.print("Button press: ");
+    #endif
+    // go through every button
+    for (uint8_t i=0; i<TRELLIS_NUM_KEYS; i++) {
+      // if it was pressed, turn it on
+      if (trellis.justPressed(i)) {
+        #ifdef DEBUG
+          Serial.println(i);
+        #endif
+        if (trellis.isLED(i)) {
+          trellis.clrLED(i);
+        }
+        else {
+          trellis.setLED(i);
+        }
+        trellis.writeDisplay();
 
-    // Second row
-    case 4: // Turn to Interaction && Thermo level 4 && Fan dial 3
-      // Set case lights to fire
-      setThermo(5);
-      setStepper(1);
-    break;
-    case 5: // Turn to Spread && pull bolt
-      setStepper(1);
-      fireLEDs(false);
-      isFire = true;
-    break;
-    case 6: // Turn to blank
-      setStepper(1);
-    break;
-
-    // Reset button
-    case 15:
-      // TODO: reset stepper, turn off LEDs
-      setHexLEDs(false);
-    break;
-  }
-  if (isFire && loopi % 20 == 0) {
-    fireLEDs(true);
+        switch(i) {
+          // First row
+          case 0: // Turn to Ignitions
+            setStepper(1);
+            setThermo(1);
+          break;
+          case 1: // Thermo level 1
+            setThermo(2);
+          break;
+          case 2: // Turns to Creativity && Thermo level 2 && Fan dial 1
+            setThermo(3);
+            setStepper(1);
+          break;
+          case 3: // Turn to Oppertunity && Thermo level 3 && Fan dial 2
+            setThermo(4);
+            setStepper(1); 
+          break;
+      
+          // Second row
+          case 4: // Turn to Interaction && Thermo level 4 && Fan dial 3
+            // Set case lights to fire
+            setThermo(5);
+            setStepper(1);
+          break;
+          case 5: // Turn to Spread && pull bolt
+            setStepper(1);
+            fireLEDs(false);
+            isFire = true;
+          break;
+          case 6: // Turn to blank
+            setStepper(1);
+          break;
+      
+          // Reset button
+          case 15:
+            #ifdef DEBUG
+              Serial.print("current stepper: ");
+              Serial.println(currentStepper);
+            #endif
+            setAll(0,0,0);
+            setHexLEDs(true);
+            setStepper(currentStepper, !STEPPER_FORWARD);
+          break;
+        }
+        
+      }
+     if (trellis.justReleased(i)) {
+        trellis.clrLED(i);
+      }
+    }
   }
 }
 
@@ -171,6 +195,10 @@ CRGB getRandomFireColor() {
  *  false = LEDs off
  */
 void setHexLEDs(bool on) {
+  #ifdef DEBUG
+    Serial.print("set hex ");
+    Serial.println(on);
+  #endif
   if (on) {
     // Turn on
     setPortion(HEX_LED_R, HEX_LED_G, HEX_LED_B, HEX_LED_START, NUM_LEDS);
@@ -186,7 +214,13 @@ void setHexLEDs(bool on) {
  *  0-5 step for the six sided display
  */
 void setStepper(int side) {
-  Serial.println("Stepper");
+  digitalWrite(STEPPER_DIR_PIN, STEPPER_FORWARD);
+  #ifdef DEBUG
+    Serial.print("(f) Stepper dir: ");
+    Serial.print(STEPPER_FORWARD);
+    Serial.print(" side: ");
+    Serial.println(side); 
+  #endif
   // 200 steps = 1 rev
   // One step = about 33 steps
   currentStepper++;
@@ -202,25 +236,31 @@ void setStepper(int side) {
 }
 
 /**
- * Check the value of the trellis
- * return - button that was pressed (if any)
- *  0-15 for a single trellis board
+ * Set the stepper motor to rotate
+ * int side - side to display
+ *  0-5 step for the six sided display
+ * bool dir - direction
  */
-int checkTrellis() {
-  if (trellis.readSwitches()) {
-    // go through every button
-    for (uint8_t i=0; i<TRELLIS_NUM_KEYS; i++) {
-      // if it was pressed, turn it on
-      if (trellis.justPressed(i)) {
-        Serial.print("Just press: ");
-        Serial.println(i);
-        trellis.setLED(i);
-        trellis.writeDisplay();
-        return i;
-      }
-    }
+void setStepper(int side, bool dir) {
+  #ifdef DEBUG
+    Serial.print("Stepper dir: ");
+    Serial.print(dir);
+    Serial.print(" side: ");
+    Serial.println(side);    
+  #endif
+  digitalWrite(STEPPER_DIR_PIN, dir);
+  // 200 steps = 1 rev
+  // One step = about 33 steps
+  currentStepper++;
+  currentStepper = currentStepper % 6;
+  int steps = 33 * side;
+  enableStepper(true);
+  for(int x = 0; x < steps; x++) {
+    digitalWrite(STEPPER_PIN,HIGH);
+    delay(10);
+    digitalWrite(STEPPER_PIN,LOW);
   }
-  return -1;
+  enableStepper(false);
 }
 
 /**
@@ -276,7 +316,9 @@ void setPortion(int r, int g, int b, int start, int finish) {
     Serial.print(" | ");
     Serial.println(finish);
   #endif
-  fill_solid( &(leds[start]), finish, CRGB( r, g, b) );
+  for (int i = start; i < finish; i++) {
+    leds[i].setRGB( r, g, b);
+  }
   FastLED.show();
 }
 
@@ -288,10 +330,12 @@ void setPortion(int r, int g, int b, int start, int finish) {
  * int finish - last led
  */
 void setPortion(int r, int g, int b, int start, int finish, int wait) {
-  Serial.print("setPortion");
-  Serial.print(start);
-  Serial.print(" | ");
-  Serial.println(finish);
+  #ifdef DEBUG
+    Serial.print("setPortion");
+    Serial.print(start);
+    Serial.print(" | ");
+    Serial.println(finish);
+  #endif
   for (int i = start; i < finish; i++) {
     leds[i].setRGB( r, g, b);
     FastLED.show();
@@ -317,6 +361,10 @@ void setThermo(int level) {
     setPortion(0, 0, 0, 0, LED_START);
   } else {
     // Set thermo leds
-    setPortion(THERMO_COLOR_R, THERMO_COLOR_G, THERMO_COLOR_B, THERMO_START, THERMO_LEVELS[level-1], THERMO_DELAY);
+    int lowBound = THERMO_START;
+    if (level > 1) {
+      lowBound = THERMO_LEVELS[level-2];
+    }
+    setPortion(THERMO_COLOR_R, THERMO_COLOR_G, THERMO_COLOR_B, lowBound, THERMO_LEVELS[level-1], THERMO_DELAY);
   }
 }
