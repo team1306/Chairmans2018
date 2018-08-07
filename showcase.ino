79a6c9b6d472dfde36f9e0bce2f7cf4565b1bcc6
Mateo Silver <mateonsilver@gmail.com>
2018-08-07 18:37:26 -0500

    Egan fixed the visual again
    He's the only one who still cares about chairmans and he quit.

--- showcase.ino
+++ showcase.ino
@@ -1,7 +1,7 @@
-#include "FastLED.h"
-
+#include <FastLED.h>
+#include <Wire.h>
 // How many leds in your strip?
-#define NUM_LEDS 231
+#define NUM_LEDS 235 
 #define DATA_PIN 12
 #define FORWARD 0
 #define BACKWARD 1
@@ -9,6 +9,14 @@
 #define MEDIUM 50
 #define FAST 5
 
+const int HEX_LEDS_START = 211;  // TODO: figure this out
+const int HEX_LED_R = 255;
+const int HEX_LED_G = 255;
+const int HEX_LED_B = 255;
+
+// Thermometer
+const int THERMO_START = 0; // First thermo led
+const int THERMO_END=31;// Last thermo led
 CRGB leds[NUM_LEDS];
 
 boolean direction = FORWARD;
@@ -20,62 +28,57 @@ void setup() {
 }
 
 void loop() { 
-
-  rainbow(1,FAST);
-  delay(3000);
-  colorWipe(CRGB::Black,FORWARD,FAST);
-  allRandom();
-  delay(3000);
-  disolve(15,100,MEDIUM);
-
-  for(int i=0; i<3; i++){
-    CRGB c1 = randomColor();
-    CRGB c2 = randomColor();
-    stripes(c1,c2,5);
-    delay(2000);
-    stripes(c2,c1,5);
-    delay(2000);
+  for(int i=0;i<255;i++){
+    allColor(Wheel(i));
+    delay(10);
+   }
+    for(int i=0;i<255;i++){
+    allColor(Wheel(254-i));
+    delay(10);
+   }
+  
+  CRGB colors[5];
+  for(int i=0;i<5;i++){
+    colors[i]=randomColor();
   }
-
-  for(int i=0; i<2; i++){
-    cylon(randomColor(), 10,FAST);
+  explosion(colors,5,FAST,false);
+  for(int i=0;i<5;i++){
+    colors[i]=randomColor();
   }
-
-
-  lightning(NULL,15,50,MEDIUM);
-  lightning(CRGB::White,20,50,MEDIUM);
-
-  for(int i=0; i<3; i++){
-    theaterChase(randomColor(),10,SLOW);
+  explosion(colors,5,FAST,false);
+  for(int i=0;i<5;i++){
+    colors[i]=randomColor();
   }
+  explosion(colors,5,FAST,false);
 
-  theaterChaseRainbow(1,MEDIUM);
+  cylon(randomColor(),10,FAST);
+  cylon(randomColor(),10,FAST);
 
-  rainbow(FAST,1);
-
-  flash(randomColor(),10,SLOW);
-
-  flash(NULL,50,MEDIUM);
-
-  for(int i=0; i<2; i++){ 
-    colorWipe(randomColor(),FAST,direction);
-    direction = !direction;
+  for(int i=0;i<3;i++){
+    CRGB c=randomColor();
+    colorWipe(c,FAST,FORWARD);
+    colorWipe(CRGB::Black,FAST,FORWARD);
   }
-
-
+   for(int i=0;i<3;i++){
+    CRGB c=randomColor();
+    colorWipe(c,FAST,BACKWARD);
+    colorWipe(CRGB::Black,FAST,BACKWARD);
+  }
+  rainbow(3,FAST,FORWARD);
+   theaterChaseRainbow(1,  MEDIUM);
 }
 
 // Changes all LEDS to given color
 void allColor(CRGB c){
   for(int i=0; i<NUM_LEDS; i++){
-    leds[i] = c;
+    setLEDs(i,c);
   }
   FastLED.show();
 }
 
 void allRandom(){
   for(int i=0; i<NUM_LEDS; i++){
-    leds[i] = randomColor();
+   setLEDs(i, randomColor());
   }
   FastLED.show(); 
 }
@@ -85,7 +88,7 @@ void disolve(int simultaneous, int cycles, int speed){
   for(int i=0; i<cycles; i++){
     for(int j=0; j<simultaneous; j++){
       int idx = random(NUM_LEDS);
-      leds[idx] = CRGB::Black;
+      setLEDs(idx,CRGB::Black);
     }
     FastLED.show();
     delay(speed);
@@ -114,10 +117,10 @@ void flash(CRGB c, int count, int speed){
 void colorWipe(CRGB c, int speed, int direction){
   for(int i=0; i<NUM_LEDS; i++){
     if(direction == FORWARD){
-      leds[i] = c;
+      setLEDs(i,c);
     }
     else{
-      leds[NUM_LEDS-1-i] = c;
+      setLEDs(NUM_LEDS-1-i, c);
     }
     FastLED.show();
     delay(speed);
@@ -125,22 +128,17 @@ void colorWipe(CRGB c, int speed, int direction){
 }
 
 // Rainbow colors that slowly cycle across LEDs
-void rainbow(int cycles, int speed){ // TODO direction
-  if(cycles == 0){
-    for(int i=0; i< NUM_LEDS; i++) {
-      leds[i] = Wheel(((i * 256 / NUM_LEDS)) & 255);
-    }
-    FastLED.show();
-  }
-  else{
+void rainbow(int cycles, int speed,int direction){ // TODO direction
     for(int j=0; j<256*cycles; j++) {
       for(int i=0; i< NUM_LEDS; i++) {
-        leds[i] = Wheel(((i * 256 / NUM_LEDS) + j) & 255);
+        if(direction==FORWARD){
+          setLEDs(i, Wheel(((i * 256 / NUM_LEDS) + j) % 255));}
+        else{
+          setLEDs(i,Wheel(((i * 256 / NUM_LEDS) + j) % 255));}   
       }
       FastLED.show();
       delay(speed);
     }
-  }
 }
 
 // Theater-style crawling lights
@@ -150,14 +148,14 @@ void theaterChase(CRGB c, int cycles, int speed){ // TODO direction
     for (int q=0; q < 3; q++) {
       for (int i=0; i < NUM_LEDS; i=i+3) {
         int pos = i+q;
-        leds[pos] = c;    //turn every third pixel on
+        setLEDs(pos, c);    //turn every third pixel on
       }
       FastLED.show();
 
       delay(speed);
 
       for (int i=0; i < NUM_LEDS; i=i+3) {
-        leds[i+q] = CRGB::Black;        //turn every third pixel off
+        setLEDs(i+q,CRGB::Black);        //turn every third pixel off
       }
     }
   }
@@ -165,18 +163,22 @@ void theaterChase(CRGB c, int cycles, int speed){ // TODO direction
 
 // Theater-style crawling lights with rainbow effect
 void theaterChaseRainbow(int cycles, int speed){ // TODO direction, duration
-  for (int j=0; j < 256 * cycles; j++) {     // cycle all 256 colors in the wheel
+  for (int j=0; j < 256 * cycles/3; j++) {     // cycle all 256 colors in the wheel
     for (int q=0; q < 3; q++) {
       for (int i=0; i < NUM_LEDS; i=i+3) {
         int pos = i+q;
-        leds[pos] = Wheel( (i+j) % 255);    //turn every third pixel on
+        byte wheel=-3*i-3*j;
+        while(wheel<0){
+          wheel=wheel+255;
+        }
+        setLEDs(pos,Wheel(wheel % 255));    //turn every third pixel on
       }
       FastLED.show();
 
       delay(speed);
 
       for (int i=0; i < NUM_LEDS; i=i+3) {
-        leds[i+q] = CRGB::Black;  //turn every third pixel off
+        setLEDs(i+q, CRGB::Black);  //turn every third pixel off
       }
     }
   }
@@ -190,12 +192,12 @@ void lightning(CRGB c, int simultaneous, int cycles, int speed){
     for(int j=0; j<simultaneous; j++){
       int idx = random(NUM_LEDS);
       flashes[j] = idx;
-      leds[idx] = c ? c : randomColor();
+      setLEDs(idx,c ? c : randomColor());
     }
     FastLED.show();
     delay(speed);
     for(int s=0; s<simultaneous; s++){
-      leds[flashes[s]] = CRGB::Black;
+      setLEDs(flashes[s], CRGB::Black);
     }
     delay(speed);
   }
@@ -206,14 +208,14 @@ void cylon(CRGB c, int width, int speed){
   // First slide the leds in one direction
   for(int i = 0; i <= NUM_LEDS-width; i++) {
     for(int j=0; j<width; j++){
-      leds[i+j] = c;
+      setLEDs(i+j, c);
     }
 
     FastLED.show();
 
     // now that we've shown the leds, reset to black for next loop
     for(int j=0; j<5; j++){
-      leds[i+j] = CRGB::Black;
+      setLEDs(i+j, CRGB::Black);
     }
     delay(speed);
   }
@@ -221,11 +223,11 @@ void cylon(CRGB c, int width, int speed){
   // Now go in the other direction.  
   for(int i = NUM_LEDS-width; i >= 0; i--) {
     for(int j=0; j<width; j++){
-      leds[i+j] = c;
+      setLEDs(i+j, c);
     }
     FastLED.show();
     for(int j=0; j<width; j++){
-      leds[i+j] = CRGB::Black;
+      setLEDs(i+j,CRGB::Black);
     }
 
     delay(speed);
@@ -236,10 +238,10 @@ void cylon(CRGB c, int width, int speed){
 void stripes(CRGB c1, CRGB c2, int width){
   for(int i=0; i<NUM_LEDS; i++){
     if(i % (width * 2) < width){
-      leds[i] = c1;
+      setLEDs(i,c1);
     }
     else{
-      leds[i] = c2;
+      setLEDs(i, c2);
     } 
   }
   FastLED.show();
@@ -249,23 +251,57 @@ void stripes(CRGB c1, CRGB c2, int width){
 void stripesChase(CRGB c1, CRGB c2, int width, int cycles, int speed){ // TODO direction
 
 }
-
+void explosion(CRGB colors[],int arr_length, int speed,boolean fade){
+  for(int j=0;j<(NUM_LEDS)/2+2;j++){
+  for(int i=0;i<j;i++){
+    CRGB c=colors[((j-i)/10)%arr_length];
+     setLEDs(NUM_LEDS/2+i,c);
+     setLEDs(NUM_LEDS/2-i,c);
+   
+  }
+  FastLED.show();
+  delay(speed);
+  }
+  if(fade){//if fade set all leds to black using a sliding blackout
+   for(int j=0;j<(NUM_LEDS)/2+2;j++){
+  for(int i=0;i<j;i++){
+    CRGB c=CRGB::Black;
+      setLEDs(NUM_LEDS/2+i,c);
+     setLEDs(NUM_LEDS/2-i,c);
+  }
+  FastLED.show();
+  delay(speed);
+  }
+  }
+}
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 CRGB Wheel(byte WheelPos) {
   if(WheelPos < 85) {
-    return CRGB(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return CRGB(1*(WheelPos * 3)/4, 1*(255 - WheelPos * 3)/4, 0);
   } 
   else if(WheelPos < 170) {
     WheelPos -= 85;
-    return CRGB(255 - WheelPos * 3, 0, WheelPos * 3);
+    return CRGB(1*(255 - WheelPos * 3)/4, 0, 1*(WheelPos * 3)/4);
   } 
   else {
     WheelPos -= 170;
-    return CRGB(0, WheelPos * 3, 255 - WheelPos * 3);
+    return CRGB(0, 1*(WheelPos * 3)/4, 1*(255 - WheelPos * 3)/4);
   }
 }
 
 CRGB randomColor(){
   return Wheel(random(256)); 
+}
+void setLEDs(int index,CRGB value){
+  index=(122+index)%NUM_LEDS;
+  if(index+1>THERMO_START&&index<THERMO_END){
+    //if it would be written to the thermometor, dont
+  }else if(index+1>HEX_LEDS_START-(THERMO_END-THERMO_START)&&index<HEX_LEDS_START){
+    //if it would be written to the leds behind the thermometer ,write it to the thermometer and its original location
+    leds[index]=value;
+    leds[HEX_LEDS_START-index]=value;
+  }else{
+      leds[index]=value;
+  }
 }
